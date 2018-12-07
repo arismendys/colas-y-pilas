9c694f4046d7803f07d0acf4f02a6855edd89089
Arismendys Marte <arismendys@gmail.com>
2018-12-07 12:39:05 -0400

    Update Lista.java

--- Lista.java
+++ Lista.java
@@ -1,3 +1,20 @@
+/*
+Esta clase nos serve para crear una lista de nodos en la cual siempre conoceremos cual es el nodo siguiente al que apunta el
+nodo en que estamos, tambien se conoce como lista simple.
+
+* El metodo getHead nos retorna el primer nodo de nuestra lista.
+* El metodo Agregar nos añade un nodo al final de la lista, pero al mismo tiempo le indica al ultimo nodo de la lista que debe 
+aputar hacia el nodo que le estamos añadiendo y nos va creando un contador de los nodos que hemos añadidos.
+* El metodo AgregarPrimero nos añade un nodo al inicio de la lista y al mismo tiempo le indica al nodo que estamos añadiendo
+que debe aputar hacia el nodo que anteriormente era quien encabezaba la lista.
+* El metodo get nos permite retornar un nodo de una posicion especifica de la lista al recibir un valor entero, su implementacion
+en otra clase para una variable tipo lista a la cual llamaremos l seria "l.get(valor del la posicion del nodo)", podemos acompañarlo
+del metodo getData de la clase nodo de la siguiente manera "l.get(1).getData()" de esta manera retornariamos dato almacenado en el
+nodo en la posicion indicada de la lista.
+* El metodo Eliminar nos permite eliminar un nodo de la lista y al mismo tiempo reduce el tamaño de la lista.
+* El metodo size nos retornara el tamaño de la lista.
+
+*/
 public class Lista {
     private Nodo head;
     private int cant = 0;
