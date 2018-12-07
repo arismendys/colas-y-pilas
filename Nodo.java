bb5fcc940ed13be20cf89da66acffe4b43aa5ae8
Arismendys Marte <arismendys@gmail.com>
2018-12-07 12:47:01 -0400

    Update Nodo.java

--- Nodo.java
+++ Nodo.java
@@ -1,3 +1,14 @@
+/*
+ Esta clase nos permitira crear nuestros Nodos en las deferentes clases que necesitemos.
+
+* El metodo setData recibe un dato tipo Object el cual se almacenara en el nodo
+* El metodo setNext recibe el nodo que ira despues del nodo actual
+* El metodo setBefore recibe el nodo antorior al que apuntara el actual
+* El metodo getData retorna un tipo Object con los datos contenidos en un nodo
+* El metodo getBefore retorna el nodo anterior al nodo actual
+* El metodo getNext retorna al nodo en la posicion siguiente al nodo actual 
+ 
+*/
 public class Nodo {
     private Object data;
     private Nodo siguiente,anterior;
